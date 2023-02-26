dc0475d57ec42931c1d809d05ed89ac9ecab253e
Oluwasegun Ariyo <oluwasegunayobami7@gmail.com>
2023-02-26 22:58:31 +0000

    first commit and implementation without unit test

---
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -0,0 +1,85 @@
+package uk.gov.dwp.uc.pairtest;
+
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
+public class TicketServiceImpl implements TicketService {
+    /**
+     * Should only have private methods other than the one below.
+     */
+
+    private final SeatReservationService seatReservationService;
+    private final TicketPaymentService ticketPaymentService;
+
+    public TicketServiceImpl (SeatReservationService seatReservationService, TicketPaymentService ticketPaymentService){
+        this.seatReservationService = seatReservationService;
+        this.ticketPaymentService = ticketPaymentService;
+    }
+
+    @Override
+    public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+        if(accountId <= 0)
+            throw new InvalidPurchaseException("Insufficient Account balance");
+
+        if(!CheckIfThereExistAdult(ticketTypeRequests))
+            throw new InvalidPurchaseException("an adult must exist to be able to purchase ticket for infant and child");
+
+        if(CalculateTotalTickets(ticketTypeRequests) > 20)
+            throw new InvalidPurchaseException("No of purchased tickets greater than 20");
+
+        int totalSeats = CalculateTotalSeatToAllocate(ticketTypeRequests);
+        int totalPayableAmount = CalculateTotalAmountToPay(ticketTypeRequests);
+
+        if (totalSeats != 0 && totalPayableAmount != 0){
+            seatReservationService.reserveSeat(accountId, totalSeats);
+            ticketPaymentService.makePayment(accountId, totalPayableAmount);
+        }
+
+    }
+
+    private int CalculateTotalSeatToAllocate(TicketTypeRequest... ticketTypeRequests){
+        int totalSeatToAllocate = 0;
+        for (TicketTypeRequest ticketTypeRequest: ticketTypeRequests) {
+            if(!ticketTypeRequest.getTicketType().equals(TicketTypeRequest.Type.INFANT)){
+                totalSeatToAllocate += ticketTypeRequest.getNoOfTickets();
+            }
+        }
+
+        return totalSeatToAllocate;
+    }
+
+    private int CalculateTotalAmountToPay(TicketTypeRequest... ticketTypeRequests){
+        int totalAmountToPay = 0;
+        for (TicketTypeRequest ticketTypeRequest: ticketTypeRequests) {
+            if(!ticketTypeRequest.getTicketType().equals(TicketTypeRequest.Type.INFANT)){
+                totalAmountToPay += ticketTypeRequest.getTicketType().equals(TicketTypeRequest.Type.ADULT)
+                        ? 20 * ticketTypeRequest.getNoOfTickets()
+                        : 10 * ticketTypeRequest.getNoOfTickets();
+            }
+        }
+
+        return totalAmountToPay;
+    }
+
+    private boolean CheckIfThereExistAdult(TicketTypeRequest... ticketTypeRequests){
+        for (TicketTypeRequest ticketTypeRequest: ticketTypeRequests) {
+            if(ticketTypeRequest.getTicketType().equals(TicketTypeRequest.Type.ADULT)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int CalculateTotalTickets(TicketTypeRequest... ticketTypeRequests){
+        int totalTickets = 0;
+        for (TicketTypeRequest ticketTypeRequest: ticketTypeRequests) {
+            if(!ticketTypeRequest.getTicketType().equals(TicketTypeRequest.Type.INFANT)){
+                totalTickets += ticketTypeRequest.getNoOfTickets();
+            }
+        }
+
+        return totalTickets;
+    }
+}
---
+++ src/test/java/TicketServiceImplTests.java
@@ -0,0 +1,11 @@
+import uk.gov.dwp.uc.pairtest.TicketService;
+import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
+
+public class TicketServiceImplTests {
+    private TicketServiceImpl ticketServiceImpl;
+    private TicketService ticketService;
+
+    public void setup(){
+
+    }
+}
